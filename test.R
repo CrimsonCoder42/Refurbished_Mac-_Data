5b6ba6350069bc77c440ec2dc760912364975b33
nostro37 <53884312+nostro37@users.noreply.github.com>
2021-11-19 13:18:09 -0500

    Add files via upload

---
+++ test.R
@@ -0,0 +1,31 @@
+library(resampledata)
+library(MASS)
+DF <- read.csv("cars.csv", stringsAsFactors = FALSE);DF
+DF$price
+
+DF[,1]
+
+mtcars
+
+
+data(package = .packages(all.available = TRUE))
+dset <- get("MplsStops")
+
+data(package = "MASS")
+
+USArrests
+
+Beerwings
+
+WeightLoss
+
+Illiteracy
+
+NCBirths2004
+
+TXBirths2004
+
+?Spruce
+
+dset <- get("Spruce")
+dset[,c(1,3,8,9)]
